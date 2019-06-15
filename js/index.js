d17cfcdc6a7452f2a5aa8f3c08a42ffac206f2d7
lechaps <romain_chapon@hotmail.com>
2019-06-15 11:51:22 +0200

    fix social media link

--- js/index.js
+++ js/index.js
@@ -70,8 +70,8 @@ Vue.component('component-cards', {
                                 <div class="uk-grid uk-grid-small uk-grid-divider uk-flex uk-flex-middle" data-uk-grid>
                                     <component-cards-vote v-bind:title="title"></component-cards-vote>                             
                                     <div class="uk-width-auto uk-text-right">
-                                        <a :href="'https://twitter.com/intent/tweet/?url=www.lejeudutitre.com&text='+title.title+'%0A&via=lejeudutitre'" target="_blank" data-uk-tooltip="title: Partager sur Twitter"  rel="nofollow" class="uk-icon-link" data-uk-icon="icon:twitter; ratio: 0.8"></a>
-                                        <a :href="'https://www.facebook.com/sharer.php?u=www.lejeudutitre.com&t='+title.title+'%0A'"                     target="_blank" data-uk-tooltip="title: Partager sur Facebook" rel="nofollow" class="uk-icon-link" data-uk-icon="icon:facebook; ratio: 0.8"></a>
+                                        <a :href="'https://twitter.com/intent/tweet/?url=www.lejeudutitre.com&text='+title.label+'%0A&via=lejeudutitre'" target="_blank" data-uk-tooltip="title: Partager sur Twitter"  rel="nofollow" class="uk-icon-link" data-uk-icon="icon:twitter; ratio: 0.8"></a>
+                                        <a :href="'https://www.facebook.com/sharer.php?u=www.lejeudutitre.com&t='+title.label+'%0A'"                     target="_blank" data-uk-tooltip="title: Partager sur Facebook" rel="nofollow" class="uk-icon-link" data-uk-icon="icon:facebook; ratio: 0.8"></a>
                                     </div>
                                 </div>
                             </div>
